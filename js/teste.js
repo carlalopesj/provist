705b3c726da81987f3e0d3eb469e90331ac4efd1
Carla Lopes Jovito <carlaljovito@gmail.com>
2023-10-26 12:17:41 -0300

    Correções das categorias e ajustes

--- js/teste.js
+++ js/teste.js
@@ -1,87 +1,30 @@
-/*document.addEventListener('DOMContentLoaded', function () {
-    const botaoPostar = document.getElementById('submit');
+const params = new URLSearchParams(window.location.search);
+const categoria = params.get("categoria");
+const conteudo = document.getElementById('conteudo');
+const frase = document.getElementById('frase');
 
-    botaoPostar.addEventListener('click', function (e) {
-        e.preventDefault(); // Impede o envio do formulário
-
-        // Obtenha os valores dos campos do formulário
-        let tituloProjeto = document.getElementById('titulo').value;
-        let descricaoProjeto = document.getElementById('descricao').value;
-        let categoriaProjeto = document.getElementById('categoria').value;
-        let telefoneContato = document.getElementById('telefone').value;
-        let emailContato = document.getElementById('email').value;
-        let arquivoProjeto = document.getElementById('arquivo').files[0]; // Use .files[0] para obter o arquivo
-
-        // Agora você pode fazer o que quiser com esses valores, por exemplo, enviá-los para o servidor usando uma solicitação AJAX
-        // ou realizando qualquer outra ação necessária.
-
-        // Exemplo: exibir os valores no console
-        console.log('Título do Projeto:', tituloProjeto);
-        console.log('Descrição do Projeto:', descricaoProjeto);
-        console.log('Categoria do Projeto:', categoriaProjeto);
-        console.log('Telefone de Contato:', telefoneContato);
-        console.log('Email de Contato:', emailContato);
-        console.log('Arquivo do Projeto:', arquivoProjeto);
-
-        // Agora você pode continuar com o processamento dos dados, como enviá-los para o servidor, validar o formulário, etc.
-
-        // Função para criar a div de projeto.
-
-    });
-});
-
-function criarProjetoDiv(dadosProjeto) {
-    // Crie a div de projeto com base nos dados e adicione-a à página.
-    const projetoDiv = document.createElement('div');
-    projetoDiv.classList.add('grid-item-a');
-    projetoDiv.innerHTML = `
-      <h2>${dadosProjeto.titulo}</h2>
-      <p>${dadosProjeto.descricao}</p>
-      <a href="individual.html" target="_blank"><button class="more">Saber mais</button></a>
-    `;
-  
-    // Adicione a div do projeto ao container (grid-container).
-    const gridContainer = document.querySelector('.grid-container-allpro');
-    gridContainer.appendChild(projetoDiv);
+if (categoria === "educacao") {
+  conteudo.innerHTML = "EDUCAÇÃO";
+  frase.innerHTML = "A educação não transforma o mundo. A educação muda as pessoas. Pessoas transformam o mundo.";
+} else if (categoria === "saude") {
+  conteudo.innerHTML = "SAÚDE";
+  frase.innerHTML = "A saúde é o bem-estar";
+} else if (categoria === "tecnologia"){
+  conteudo.innerHTML = "TECNOLOGIA";
+  frase.innerHTML = "Tecnologia parara";
+} else if (categoria === "meio-ambiente") {
+  conteudo.innerHTML = "MEIO-AMBIENTE";
+  frase.innerHTML = "Proteger a natureza não é uma escolha, é nossa responsabilidade compartilhada. Pequenas ações diárias geram grandes impactos para um futuro sustentável.";
+} else if (categoria === "financas") {
+  conteudo.innerHTML = "FINCANÇAS";
+  frase.innerHTML = "Finançassssa s";
+} else if (categoria === "entretenimento") {
+  conteudo.innerHTML = "ENTRETENIMENTO";
+  frase.innerHTML = "Entretenimento caidasdasd";
+} else if (categoria === "seguranca") {
+  conteudo.innerHTML = "SEGURANÇA";
+  frase.innerHTML = "Segurançasasd s";
+} else if (categoria === "outros") {
+  conteudo.innerHTML = "OUTROS";
+  frase.innerHTML = "Não seja igual aos outros, faça e viva o diferente.";
 }
-  
-  // Função para obter parâmetros de consulta da URL.
-  function obterParametrosDaURL() {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    return {
-      titulo: urlParams.get('titulo'),
-      descricao: urlParams.get('descricao')
-    };
-}
-  
-  // Verifique a URL da página atual e tome ação com base nela.
-  if (window.location.pathname === '/index2.html') {
-    // Estamos na página de perfil/formulário.
-    const btnPostar = document.getElementById('submit');
-  
-    btnPostar.addEventListener('click', function(event) {
-      event.preventDefault();
-  
-      // Obtenha os valores dos campos do formulário.
-      const tituloProjeto = document.getElementById('titulo').value;
-      const descricaoProjeto = document.getElementById('descricao').value;
-  
-      // Crie uma URL com os parâmetros de consulta.
-      const url = `propage.html?titulo=${encodeURIComponent(tituloProjeto)}&descricao=${encodeURIComponent(descricaoProjeto)}`;
-  
-      // Redirecione para a página de exposição de projetos.
-      window.location.href = url;
-    });
-  } else if (window.location.pathname === '/propage.html') {
-    // Estamos na página de exposição de projetos.
-    const dadosProjeto = obterParametrosDaURL();
-    criarProjetoDiv(dadosProjeto);
-  }
-  
-*/
-//Tentando coisar os formulários para a página
-
-
-
-
