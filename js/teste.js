61aad8e4f452cacc227d67551e6f0f13464903be
Carla Lopes Jovito <carlaljovito@gmail.com>
2023-11-01 10:41:51 -0300

    Atualizações

--- js/teste.js
+++ js/teste.js
@@ -8,23 +8,23 @@ if (categoria === "educacao") {
   frase.innerHTML = "A educação não transforma o mundo. A educação muda as pessoas. Pessoas transformam o mundo.";
 } else if (categoria === "saude") {
   conteudo.innerHTML = "SAÚDE";
-  frase.innerHTML = "A saúde é o bem-estar";
+  frase.innerHTML = "Saúde é o estado de completo bem-estar físico, mental e social e não somente a ausência de doença.";
 } else if (categoria === "tecnologia"){
   conteudo.innerHTML = "TECNOLOGIA";
-  frase.innerHTML = "Tecnologia parara";
+  frase.innerHTML = "A tecnologia é a poesia do futuro, escrita em linhas de código e moldando o amanhã.";
 } else if (categoria === "meio-ambiente") {
   conteudo.innerHTML = "MEIO-AMBIENTE";
   frase.innerHTML = "Proteger a natureza não é uma escolha, é nossa responsabilidade compartilhada. Pequenas ações diárias geram grandes impactos para um futuro sustentável.";
 } else if (categoria === "financas") {
-  conteudo.innerHTML = "FINCANÇAS";
+  conteudo.innerHTML = "A economia é uma virtude distributiva e consiste não em poupar mas em escolher.";
   frase.innerHTML = "Finançassssa s";
 } else if (categoria === "entretenimento") {
   conteudo.innerHTML = "ENTRETENIMENTO";
-  frase.innerHTML = "Entretenimento caidasdasd";
+  frase.innerHTML = "O entretenimento é a mágica que nos transporta para outros mundos sem sair do lugar.";
 } else if (categoria === "seguranca") {
   conteudo.innerHTML = "SEGURANÇA";
-  frase.innerHTML = "Segurançasasd s";
+  frase.innerHTML = "Acidentes não acontecem por acaso, mas sim, por descado.";
 } else if (categoria === "outros") {
   conteudo.innerHTML = "OUTROS";
-  frase.innerHTML = "Não seja igual aos outros, faça e viva o diferente.";
+  frase.innerHTML = " ";
 }
